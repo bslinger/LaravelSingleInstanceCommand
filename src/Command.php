b31ce29da9764108920120f26a431a87db604068
Denis <me@denismilovanov.net>
2015-06-05 15:19:04 +0300

    Cli option 'stop' to send SIGTERM

--- src/Command.php
+++ src/Command.php
@@ -21,12 +21,12 @@ class Command extends \Illuminate\Console\Command
         return sys_get_temp_dir() . '/' . env('APP_ENV') . '/';
     }
 
-    private static function shutdown($reason, $pid)
+    private static function shutdown($reason, $pid_file)
     {
         app('log')->info($reason);
-        if (file_exists($pid)) {
-            unlink($pid);
-            app('log')->info('Pid file was ' . $pid . '.');
+        if (file_exists($pid_file)) {
+            unlink($pid_file);
+            app('log')->info('Pid file was ' . $pid_file . '.');
         }
         app('log')->info('Process is going to successfully stop right now.');
         exit(0);
@@ -37,25 +37,41 @@ class Command extends \Illuminate\Console\Command
         $p = intval($input->getParameterOption('p', 1));
         $command = $input->getFirstArgument();
 
-        $pid = self::getPidFile($command, $p);
+        $pid_file = self::getPidFile($command, $p);
 
-        if (! file_exists($dir = dirname($pid))) {
+        if (! file_exists($dir = dirname($pid_file))) {
             mkdir($dir, 0775, true);
         }
 
-        if (file_exists($pid)) {
-            app('log')->info('Process is already running, pid file is ' . $pid . ', pid is ' . file_get_contents($pid) . '.');
+        if ($input->hasParameterOption('stop')) {
+            if (file_exists($pid_file)) {
+                $pid = file_get_contents($pid_file);
+                $result = posix_kill($pid, 15);
+                if ($result) {
+                    app('log')->info('Term signal has been sent, pid file is ' . $pid_file . ', pid is ' . $pid . '.');
+                } else {
+                    app('log')->info('Unable to send term signal.');
+                }
+            } else {
+                app('log')->info('There is no pid file ' . $pid_file);
+            }
+            exit(0);
+        }
+
+        if (file_exists($pid_file)) {
+            $pid = file_get_contents($pid_file);
+            app('log')->info('Process is already running, pid file is ' . $pid_file . ', pid is ' . $pid . '.');
             exit(1);
         }
 
-        file_put_contents($pid, posix_getpid());
+        file_put_contents($pid_file, posix_getpid());
 
-        $shutdownSignal15 = function() use ($pid) {
-            self::shutdown('SIGTERM received.', $pid);
+        $shutdownSignal15 = function() use ($pid_file) {
+            self::shutdown('SIGTERM received.', $pid_file);
         };
 
-        $shutdownNormal = function() use ($pid) {
-            self::shutdown('Normal termination.', $pid);
+        $shutdownNormal = function() use ($pid_file) {
+            self::shutdown('Normal termination.', $pid_file);
         };
 
         register_shutdown_function($shutdownNormal);
